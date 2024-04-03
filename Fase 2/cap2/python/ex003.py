e4d8a8870cdbb0f1be62006dd5a47115c5fe503c
vnickolas <vitornickolas10@gmail.com>
2024-04-03 13:18:06 -0300

    att

--- Fase 2/cap2/python/ex003.py
+++ Fase 2/cap2/python/ex003.py
@@ -9,21 +9,21 @@ if bpm.upper() == "N":
     bpm = int(bpm) 
     if idade <= 2 and bpm <= 140 and bpm >= 101:
         print("Seu BPM está bom. Em caso de dúvidas, consulte um médico")
-    elif idade >= 3 and bpm <= 100 and bpm >= 80:
+    elif idade >= 3 and idade <= 17 and bpm <= 100 and bpm >= 80:
         print("Seu BPM está bom. Em caso de dúvidas, consulte um médico")
-    elif idade >= 18 and bpm <= 79 and bpm >= 70:
+    elif idade >= 18 and idade <= 49 and bpm <= 79 and bpm >= 70:
         print("Seu BPM está bom. Em caso de dúvidas, consulte um médico")
-    elif idade >= 64 and bpm <= 69:
+    elif idade >= 50 and bpm <= 69:
         print("Seu BPM está bom. Em caso de dúvidas, consulte um médico")
 else: 
     bpm = int(bpm) 
 if idade <= 2 and bpm <= 140 and bpm >= 101:
     print("Seu BPM está bom. Em caso de dúvidas, consulte um médico")
-elif idade >= 3 and bpm <= 100 and bpm >= 80:
+elif idade >= 3 and idade <= 17 and bpm <= 100 and bpm >= 80:
     print("Seu BPM está bom. Em caso de dúvidas, consulte um médico")
-elif idade >= 18 and bpm <= 79 and bpm >= 70:
+elif idade >= 18 and idade <= 49 and bpm <= 79 and bpm >= 70:
     print("Seu BPM está bom. Em caso de dúvidas, consulte um médico")
-elif idade >= 64 and bpm <= 69:
+elif idade >= 50 and bpm <= 69:
     print("Seu BPM está bom. Em caso de dúvidas, consulte um médico")
 else:
     print("Seu BPM está fora do normal. Por favor, sugerimos que consulte-se com um médico.")
--- Fase 2/cap2/python/ex006.py
+++ Fase 2/cap2/python/ex006.py
@@ -1,6 +1,45 @@
-PLAYSTATION = 0
-XBOX = 0
-NITENDO = 0
+PLAYSTATION = int()
+XBOX = int()
+NITENDO = int()
+print(" 1 - (PLAYSTATION)\n 2 - (XBOX)\n 3 - (NITENDO)")
 
 for i in range(5):
-    
+    print("Digite o número do console que você deseja: ")
+    voto = int(input())  # Converte o voto para maiúsculas para facilitar a comparação
+    if voto == 1:
+        print(f"Você escolheu o console: PLAYSTATION")
+    elif voto == 2: 
+        print(f"Você escolheu o console: XBOX")
+    else:
+        print(f"Você escolheu o console: NITENDO")
+
+
+ # Verifica o voto e atualiza o contador apropriado
+    if voto == 1:
+        PLAYSTATION += 1
+    elif voto == 2:
+        XBOX += 1
+    elif voto == 3:
+        NITENDO += 1
+    else:
+        print("Voto inválido! Por favor, escolha entre PLAYSTATION, XBOX ou NINTENDO.")
+
+# Determina o console vencedor com base nos votos
+if PLAYSTATION > XBOX and PLAYSTATION > NITENDO:
+    console_vencedor = "PLAYSTATION"
+    total_votos = PLAYSTATION
+elif XBOX > PLAYSTATION and XBOX > NITENDO:
+    console_vencedor = "XBOX"
+    total_votos = XBOX
+elif NITENDO > PLAYSTATION and NITENDO > XBOX:
+    console_vencedor = "NINTENDO"
+    total_votos = NITENDO
+else:
+    console_vencedor = "EMPATE"
+    total_votos = PLAYSTATION  # ou qualquer outro contador, já que todos são iguais
+
+# Exibe o resultado
+if console_vencedor == "EMPATE":
+    print("Houve um empate entre os consoles!")
+else:
+    print(f"O console vencedor é {console_vencedor} com {total_votos} votos!")
